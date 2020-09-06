abb09af870b0817461efaf61c362816618aa8d26
Elektra-2 <carol.rolo@hotmail.com>
2020-09-06 18:41:46 -0300

    adding files and exercises chapter4 lists

---
+++ python_work/Chapter4/one_hundred.py
@@ -0,0 +1,7 @@
+# Make a list of the numbers from one to one hundred,
+# and then use a for loop to print the numbers.
+# (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
+
+hundred_numbers = list(range(1, 101))
+for number in hundred_numbers:
+    print(number)
---
+++ python_work/Chapter4/one_hundred_sum.py
@@ -0,0 +1,12 @@
+#  Make a list of the numbers from one to one hundred, and then use min() and max()
+#  to make sure your list actually starts at one and ends at one hundred.
+# Also, use the sum() function to see how quickly Python can add a million numbers.
+
+# Making a list with one hundred numbers
+one_hundred = list(range(0, 101))
+
+print(min(one_hundred))  # Printing the first number or min value
+
+print(max(one_hundred))  # Printing the last value of the list
+
+print(sum(one_hundred))  # Print the sum of the list
